e4c0c664d4f680ea2c0c094bcaf9b637b4897f34
archangelmichael01 <88234540+archangelmichael01@users.noreply.github.com>
2021-08-04 20:43:00 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,6 +1,6 @@
 
 START_MSG = """
-Hello <b>{}</b> ! I'm <b>Akinator(Unofficial)</b>.
+Hello <b>{}</b> ! I'm <b>Michael</b>.
 We can play a guessing game.
 Do <code>/play</code>
 """
@@ -18,8 +18,8 @@ ME_MSG = """
 <b>Total Questions :</b> <code>{}</code>
 """
 
-GITHUB_URL = "https://github.com/notPlasticCat"
-DEV_URL = "https://t.me/plastic_cat"
+GITHUB_URL = "t.me/SOULxDED"
+DEV_URL = "https://t.me/TheRiZoeL"
 
 AKI_LANG_CODE = {
     'en': 'English',
